d4c68cc3dcb1bde2c215be41c2535addae22abea
Daniel Rubiano <31528031+NeilPolitum@users.noreply.github.com>
2020-02-12 09:11:37 -0500

    Add files via upload

---
+++ nodos.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal
+"""
+
+class Nodo:
+    def __init__(self, info):
+        self.__info = info
+        self.__sig = None
+        
+    def info(self):
+        return self.__info
+    
+    def sig(self):
+        return self.__sig
+    
+    def cambiar_sig(self,direcc):
+        self.__sig = direcc
+        
+class Lista():
+    __cab = None
+    def __init__(self):
+        self.__cab = None
+        
+    def insertar(self, nodo):
+        if self.__cab == None:
+            self.__cab = nodo
+            return
+        
+        q = None
+        p = self.__cab
+        while p != None and p.info() < nodo.info():
+            q = p
+            p = p.sig()
+            
+        if p != None and p.info() == nodo.info():
+            print ("{nodo.info()} ya esta en la lista")
+            return
+        
+        if q == None:
+            nodo.cambiar_sig(p)
+            self.__cab = nodo
+            return
+        
+        nodo.cambiar_sig(p)
+        q.cambiar_sig(nodo)
+        
+    def escribir_lista(self):
+        p = self.__cab
+        while p != None:
+            print (p.info())
+            p = p.sig()
+            
+lista = Lista()
+dato = int(input("Digite numero a insertar. (-1) para terminar: "))
+while dato != -1:
+    nodo = Nodo(dato)
+    lista.insertar(nodo)
+    dato = int(input("Digite numero a insertar. (-1) para terminar: "))
+    
+lista.escribir_lista()
