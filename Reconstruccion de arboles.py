5e0f16e13352b3dec70849e56c5e3054bdd2a86a
Felipe Velandia <fvelandia695@gmail.com>
2020-03-17 14:38:08 -0500

    Add files via upload

--- Reconstruccion de arboles.py
+++ Reconstruccion de arboles.py
@@ -49,7 +49,7 @@ class Arbol():
             print(str(raiz.info()))
             self.inorden(raiz.rDer())
         
-    def recArbol(self, inOrder, preOrder, raiz=None):
+    def recArbolPre(self, inOrder, preOrder, raiz=None):
         if len(inOrder)!=len(preOrder):
             print("Vectores con diferentes longitudes")
             return     
@@ -68,10 +68,44 @@ class Arbol():
             if len(preOrder)>1:
                 newIn = inOrder[0:inOrder.index(info)]
                 newPre = preOrder[1:inOrder.index(info)+1]        
-                p.cambiar_izq(self.recArbol(newIn,newPre,p))
+                p.cambiar_izq(self.recArbolPre(newIn,newPre,p))
                 newIn = inOrder[inOrder.index(info)+1:]
                 newPre = preOrder[inOrder.index(info)+1:]
-                p.cambiar_der(self.recArbol(newIn,newPre,p))
+                p.cambiar_der(self.recArbolPre(newIn,newPre,p))
+                
+        else:
+            return None
+        
+        if raiz==None:
+            self.__raiz = p
+            
+        else:
+            return p
+        
+    def recArbolPost(self, inOrder, postOrder, raiz=None):
+        
+        print("-------------------------")
+        print("Post:")
+        for i in postOrder:
+            print(i)
+        
+        print("In:")
+        for i in inOrder:
+            print(i)
+        if len(inOrder)!=len(postOrder):
+            print("Vectores con diferentes longitudes")
+            return     
+            
+        if len(postOrder)>0:
+            info=postOrder[len(postOrder)-1]
+            p=Nodo(info)
+            if len(postOrder)>1:
+                newIn = inOrder[0:inOrder.index(info)]
+                newPost = postOrder[0:inOrder.index(info)]        
+                p.cambiar_izq(self.recArbolPost(newIn,newPost,p))
+                newIn = inOrder[inOrder.index(info)+1:]
+                newPost = postOrder[inOrder.index(info):-1]
+                p.cambiar_der(self.recArbolPost(newIn,newPost,p))
                 
         else:
             return None
@@ -92,21 +126,30 @@ while op != 0:
           "2. Listar en inorden\n"
           "3. Listar en preorden\n"
           "4. Listar en postorden\n"
+          "5. Insertar en post\n"
           "0. Salir")
     op = int(input("Digite la opcion: "))
     
     if(op==1):
+        ar=Arbol()
         preS = input("Preorder: ")
         inoS = input("Inorder: ")
         pre=preS.split(",")
         ino=inoS.split(",")
-        ar.recArbol(ino,pre)
+        ar.recArbolPre(ino,pre)
     elif(op==2):
         ar.inorden(ar.raizarbol())
     elif(op==3):
         ar.preorden(ar.raizarbol())
     elif(op==4):
-        ar.posorden(ar.raizarbol())            
+        ar.posorden(ar.raizarbol()) 
+    elif(op==5):
+        ar=Arbol()
+        postS = input("Postorder: ")
+        inoS = input("Inorder: ")
+        post=postS.split(",")
+        ino=inoS.split(",")
+        ar.recArbolPost(ino,post)           
     elif(op==0):
         print("Calabaza, calabaza, cada quien pa su gran puta mierda")
         
